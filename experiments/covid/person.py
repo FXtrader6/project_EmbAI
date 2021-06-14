fb79f5b6dc0c2a91e175f58d9f5a32c8def52376
F-jaber <fakhreldinjaber@gmail.com>
2021-06-14 22:47:04 +0300

    #up and running

--- experiments/covid/person.py
+++ experiments/covid/person.py
@@ -1,11 +1,125 @@
 import numpy as np
 import pygame
+import random
+import time, threading
 
-from experiments.covid.config import config
 from simulation.agent import Agent
+from experiments.covid.config import config
 from simulation.utils import *
 
 
 class Person(Agent):
     """ """
-    pass
+    def __init__(
+            self, pos, v, population, index: int, image: str = "experiments/covid/images/corona.png"
+    ) -> None:
+
+        super(Person, self).__init__(
+            pos,
+            v,
+            image,
+            max_speed=config["agent"]["max_speed"],
+            min_speed=config["agent"]["min_speed"],
+            mass=config["agent"]["mass"],
+            width=config["agent"]["width"],
+            height=config["agent"]["height"],
+            dT=config["agent"]["dt"],
+            index=index
+        )
+
+        self.population = population
+        self.t_join = 0
+        self.t_attempt_leave = 0
+        self.t_leave_site = 0
+        self.roach_timer = 0
+        self.stop = False
+        self.state = "wander"
+        self.on_site = False
+        self.n_agents = config["base"]["n_agents"]
+
+
+
+
+    def change_state(self) -> None:
+
+        if self.state == "wander":
+            self.v = self.set_velocity()
+        elif self.state == "still":
+            self.v *= 0
+
+
+    def site_behavior(self, behaviour = "join") -> None:
+        if behaviour == "join":
+            num_neighbors = len(self.population.find_neighbors(self, config["cockroach"]["radius_view"]))
+            if num_neighbors <= 5:
+                p_join = 0.5
+            elif 5 <= num_neighbors <= 10:
+                p_join = 0.8
+            else:
+                p_join = 0.99
+            if random.random() < p_join:
+               self.state = "still"
+               self.change_state()
+        elif behaviour == "leave":
+            num_neighbors = len(self.population.find_neighbors(self, config["cockroach"]["radius_view"]))
+            if num_neighbors <= 5:
+                p_leave = 0.1
+
+            elif 5 <= num_neighbors <= 10:
+                p_leave = 0.05
+            else:
+                p_leave = 0.01
+            if random.random() < p_leave:
+                self.state = "wander"
+                self.t_leave_site += 1
+
+                self.on_site = True
+                #self.timer = 0
+                self.change_state()
+
+                #self.change_state(state="wander")
+
+
+
+
+            #leave
+
+
+
+    def update_actions(self) -> None:
+            self.roach_timer += 1
+            if self.roach_timer % 6000 == 0:
+                time.sleep(45)
+
+
+            for obstacle in self.population.objects.obstacles:
+                collide = pygame.sprite.collide_mask(self, obstacle)
+                if bool(collide):
+                    self.avoid_obstacle()
+
+            #if self.min_speed != 0 and self.max_speed != 0 and self.timer2 == 0:
+
+            for site in self.population.objects.sites:
+                col = pygame.sprite.collide_mask(self, site)
+                if bool(col):
+
+                    self.t_join += 1
+
+                    if self.t_join % 35 == 0:
+                        if self.on_site == False:
+                            self.site_behavior()
+                            self.t_join = 0
+
+            if self.t_leave_site >= 1:
+                self.t_leave_site +=1
+                #print(self.timer3)
+            if self.t_leave_site > 250:
+                self.on_site = False
+                self.t_leave_site = 0
+
+            if self.state == "still":
+                self.t_attempt_leave += 1
+                #print(self.timer2)
+                if self.t_attempt_leave % 500 == 0:
+                    self.site_behavior(behaviour="leave")
+                    self.t_attempt_leave = 0
--- experiments/covid/population.py
+++ experiments/covid/population.py
@@ -2,6 +2,7 @@ from experiments.covid.config import config
 from experiments.covid.person import Person
 from simulation.swarm import Swarm
 from simulation.utils import *
+import numpy as np
 
 
 class Population(Swarm):
@@ -9,7 +10,7 @@ class Population(Swarm):
 
     def __init__(self, screen_size) -> None:
         super(Population, self).__init__(screen_size)
-        # To do
+        self.object_loc = config["population"]["outside"]
 
     def initialize(self, num_agents: int) -> None:
         """
@@ -17,6 +18,21 @@ class Population(Swarm):
             num_agents (int):
 
         """
+        if config["population"]["obstacles"]:
+            object_loc = config["base"]["object_location"]
+
+            if config["population"]["outside"]:
+                scale = [300, 300]
+            else:
+                scale = [800, 800]
+
+            filename = (
+                "experiments/covid/images/env.png"
+            )
+
+            self.objects.add_object(
+                file=filename, pos=object_loc, scale=scale, obj_type="obstacle"
+            )
 
         # To Do
         # code snipet (not complete) to avoid initializing agents on obstacles
@@ -36,3 +52,7 @@ class Population(Swarm):
                         )
                 except IndexError:
                     pass
+        for index, agent in enumerate(range(num_agents)):
+
+
+            self.add_agent(Person(pos=np.array(coordinates), v=None, population=self, index=index))
